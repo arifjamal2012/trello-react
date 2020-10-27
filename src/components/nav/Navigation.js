15bdaa40ea19c7a3a6ab6017fd4c5f87f82eaaae
Arif jamal <arifjamal2012@gmail.com>
2020-10-27 17:21:44 +0530

    added navigation bar

---
+++ src/components/nav/Navigation.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import TopBar from './TopBar';
+import SideMenu from './SideMenu';
+
+  export default function Navigation() {
+      const [openSideMenu,setOpenSideMenu] = useState(false);
+    
+      return (
+          <div>
+              <TopBar setOpenSideMenu={setOpenSideMenu}/>
+              <SideMenu openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
+          </div>
+      )
+  }
---
+++ src/components/nav/SideMenu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import { Drawer } from '@material-ui/core';
+
+const useStyles = makeStyles ((theme) => ({
+    drawer: {
+        width: '350px',
+    }
+}));
+
+export default function SideMenu ({setOpenSideMenu,openSideMenu}) {
+    const classes = useStyles();
+    return (
+        <div>
+            <Drawer 
+            open={openSideMenu} 
+            anchor="right" 
+            onClose={()=>setOpenSideMenu(false)}>
+                <div className={classes.drawer}>
+                  <h1>Hello sidemenu</h1>
+                </div>
+                
+            </Drawer>
+        </div>
+    )
+}
